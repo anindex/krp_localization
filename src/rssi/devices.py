1b673235736df406352c33f3f797d4d2b077ff3b
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2019-08-05 14:40:32 +0200

    Implement iw probe mode

--- src/rssi/devices.py
+++ src/rssi/devices.py
@@ -10,23 +10,37 @@ class WifiDevice:
     Class WifiDevice
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.probe_completed = True
+        self.mode = kwargs.get("mode", "iw")
 
     def probe_signal(self):
         self.probe_completed = False
         signals = {}
 
-        mac_cmd = 'nmcli -t -f BSSID,SIGNAL device wifi list'
-        process = subprocess.Popen(mac_cmd.split(),stdout=subprocess.PIPE)
-        process.wait()
+        if self.mode == "nmcli":
+            mac_cmd = 'nmcli -t -f BSSID,SIGNAL device wifi list'
+            process = subprocess.Popen(mac_cmd.split(),stdout=subprocess.PIPE)
+            process.wait()
 
-        for sig in process.stdout:
-            sig = sig.replace("\\", "")
-            sig = sig.strip("\n").split(":")
-            mac = ":".join(sig[:-1])
-            strength = int(sig[-1])
-            signals[mac] = strength
+            for sig in process.stdout:
+                sig = sig.replace("\\", "")
+                sig = sig.strip("\n").split(":")
+                mac = ":".join(sig[:-1])
+                strength = int(sig[-1])
+                signals[mac] = strength
+        elif self.mode == "iw":
+            mac_cmd = 'sudo iw wlx74da38d00733 scan | awk '/^BSS/{mac = gensub ( /^BSS[[:space:]]*([0-9a-fA-F:]+).*?$/, "\\1", "g", $0 );}/^[[:space:]]*signal:/{signal = gensub ( /^[[:space:]]*signal:[[:space:]]*(\-?[0-9.]+).*?$/, "\\1", "g", $0 );printf ("%s %s\n", mac, signal);}' | sort'
+            process = subprocess.Popen(mac_cmd.split(),stdout=subprocess.PIPE)
+            process.wait()
+
+            for sig in process.stdout:
+                sig = sig.strip("\n").split(" ")
+                mac = sig[0]
+                strength = int(sig[1])
+                signals[mac] = strength
+        else:
+            print("Invalid wifi measurement mode! Mode are nmcli or iw")
 
         self.probe_completed = True
         return signals
