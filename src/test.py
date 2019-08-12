a435b2524e18f640a52ccfa0fff7c3c91059d688
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2019-08-12 13:54:20 +0200

    Prepare data complete

--- src/test.py
+++ src/test.py
@@ -3,41 +3,32 @@ from __future__ import print_function
 from data_preprocessing import PreprocessedData, Measurement
 from utils.structures import mesh, pose_from_array
 from utils.load import load_data, save
+from utils.data_processing import rosbag_inspect, rosbag_croptime, rosbag_concatenate
 from localization.models import hGP
 from krp_localization.msg import RSSIData
 
 import rospkg
 
-
 rospack = rospkg.RosPack()
-train_data_path = rospack.get_path('krp_localization') + "/data/"
-raw_rssi, poses = load_data(file_name="test", file_path=train_data_path)
+file_path = rospack.get_path('krp_localization') + '/bags/test.bag'
+#rosbag_inspect(file_path)
 
-#print("Num rssi: ", len(raw_rssi))
-#print("Num poses: ", len(poses))
-#print("Num odom: ", len(odom))
-#print("Sample rssi: ", raw_rssi[0])
-#print("Sample pose: ", poses[0])
-#print("Sample odom: ", odom[0])
+output_bag1 = rospack.get_path('krp_localization') + '/bags/krp1-1.bag'
+output_bag2 = rospack.get_path('krp_localization') + '/bags/krp1-2.bag'
+output_bag3 = rospack.get_path('krp_localization') + '/bags/krp1-3.bag'
 
-#print(raw_rssi[0])
-#m = Measurement(raw_rssi[0], flag_negative_db=True)
-#print(m)
+krp_bag1    = rospack.get_path('krp_localization') + '/bags/krp1.bag'
+krp_bag2    = rospack.get_path('krp_localization') + '/bags/krp2.bag'
 
-#print("Num rssi before fuse: ", len(raw_rssi))
-#data = PreprocessedData(raw_rssi, poses=poses, flag_negative_db=True, flag_min_distance=True, flag_fuse_min_points_per_AP=True)
-#print("Num rssi after fuse: ", data.nm)
-#print(data.data['Y'][0])
-#print(data.data['Var'][0])
-#print(data.data['n'][0])
+#rosbag_croptime(file_path, output_bag1, tend=1565603530.0)
+#rosbag_croptime(file_path, output_bag2, tstart=1565603580, tend=1565603650)
+#rosbag_croptime(file_path, output_bag3, tstart=1565603680, tend=1565603720)
+#rosbag_inspect(output_bag1)
+#rosbag_inspect(output_bag2)
+#rosbag_inspect(output_bag3)
 
-start, end = poses[0].header.stamp.secs, poses[-1].header.stamp.secs
-print("Duration: {}s".format(end - start))
-print("Len RSSI: {}".format(len(raw_rssi)))
 
-inc = (end - start) / float(len(raw_rssi))
-print(inc)
-for i, m in enumerate(raw_rssi):
-    m.start_time_ns += (start + i * inc) * 1e9
-
-save(raw_rssi, "/home/anindex/robotics_ws/src/krp_localization/data/testrssi_fixed.p")
+rosbag_concatenate(output_bag1, output_bag2, krp_bag1)
+rosbag_concatenate(output_bag1, output_bag3, krp_bag2)
+rosbag_inspect(krp_bag1)
+rosbag_inspect(krp_bag2)
--- src/utils/data_processing.py
+++ src/utils/data_processing.py
@@ -1,5 +1,13 @@
 import numpy as np
 import copy
+from subprocess import check_output, call
+import rosbag, rospy
+import tf
+from geometry_msgs.msg import Transform, Quaternion
+import yaml
+
+
+
 
 def validate_data(din):
     """
@@ -32,3 +40,104 @@ def validate_data(din):
         data['Var'] = np.reshape(data['Var'],(data['Var'].shape[0],1)) #casting into [p,1]
 
     return data
+
+
+def rosbag_inspect(input_bag):
+    bag_path = input_bag
+    info_dict = yaml.load(rosbag.bag.Bag(bag_path, 'r')._get_yaml_info())
+    print(bag_path)
+    print('Start    \t: '+str(info_dict['start']))
+    print('End      \t: '+str(info_dict['end']))
+    duration = info_dict['end']-info_dict['start']
+    print('Messages \t: '+str(info_dict['messages']))
+    topics = info_dict['topics']
+    #Uncomment the following if you want to see information about the topics
+    print('(#messages) \ttopic')
+    for topic in topics:
+        print('  ('+str(topic['messages'])+')    \t'+topic['topic'])
+
+def rosbag_croptime(input_bag, output_bag, tstart=0, tend=2e9):
+    filter_cmd = "({}<=t.secs)and(t.secs<{})".format(tstart,tend)
+    call(['rosbag','filter',input_bag,output_bag,filter_cmd])
+
+def rosbag_concatenate(input_bag1,input_bag2,output_bag):
+    #Find end of the first bag and beginning of the second
+    #First bag must end before the beginning of the second
+    info_dict = yaml.load(rosbag.bag.Bag(input_bag1, 'r')._get_yaml_info())
+    bag1_tend = info_dict['end']
+    info_dict = yaml.load(rosbag.bag.Bag(input_bag2, 'r')._get_yaml_info())
+    bag2_tstart = info_dict['start']
+    offset_time = bag2_tstart-bag1_tend+0.01
+    offset_time = rospy.rostime.Time.from_sec(offset_time)
+
+    pr = 1
+    seq = 0
+    with rosbag.bag.Bag(output_bag, 'w') as outbag:
+        #The first input bag is directly copied into output bag
+        for topic, msg, t in rosbag.bag.Bag(input_bag1):
+            outbag.write(topic, msg, t)
+            if topic=='rosout':
+                seq = msg.header.seq
+
+            if topic=='tf':
+                if t.to_sec()>bag1_tend-1.:
+                    tr1 = msg.transforms[0].transform
+            outbag.write(topic, msg, t)
+
+        #The second input bag's time is modified as well as its odometry TF
+        for topic, msg, t in rosbag.bag.Bag(input_bag2).read_messages(topics=['tf', 'tf_static', 'rssi', 'amcl_pose', 'base_scan', 'odom']):
+            #new time
+            tnew = rospy.rostime.Time.from_sec((t-offset_time).to_sec())
+            if topic=='tf':
+                # considerations: only odometry is corrected and
+                #                 odometry is the only transform,
+                if pr:
+                    # first tf
+                    tr2 = msg.transforms[0].transform
+                    pr = 0
+                trn = msg.transforms[0].transform
+                msg.transforms[0].transform = transform_fix(tr1,tr2,trn)
+
+                for transform in msg.transforms:
+                    transform.header.stamp = tnew
+            else:
+                try:
+                    msg.header.stamp = tnew
+                except:
+                    pass
+
+            if topic=='rosout':
+                seq += 1
+                msg.header.seq = seq
+            if topic=='rssi':
+                msg.start_time_ns = msg.start_time_ns-long(offset_time.to_nsec())
+
+            outbag.write(topic, msg, tnew)
+
+
+def transform_fix(t1,t2,tn):
+    """
+    t1,t2,tn
+    """
+    #translation
+    dx = tn.translation.x - t2.translation.x
+    dy = tn.translation.y - t2.translation.y
+
+    #rotation
+    euler1 = tf.transformations.euler_from_quaternion([t1.rotation.x, t1.rotation.y, t1.rotation.z, t1.rotation.w])
+    euler2 = tf.transformations.euler_from_quaternion([t2.rotation.x, t2.rotation.y, t2.rotation.z, t2.rotation.w])
+    eulern = tf.transformations.euler_from_quaternion([tn.rotation.x, tn.rotation.y, tn.rotation.z, tn.rotation.w])
+
+    dyaw = euler2[2]-euler1[2]
+
+    ndx = dx*np.cos(dyaw)+dy*np.sin(dyaw)
+    ndy = -dx*np.sin(dyaw)+dy*np.cos(dyaw)
+
+    tout = Transform()
+    tout.translation.x = ndx+t1.translation.x
+    tout.translation.y = ndy+t1.translation.y
+
+    qout = tf.transformations.quaternion_from_euler(0,0,eulern[2]-euler2[2]+euler1[2])
+    tout.rotation = Quaternion(*qout)
+
+    return tout
