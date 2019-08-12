3dc24e7d0aa34f631e3593afe815da405e32e491
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2019-08-12 22:28:49 +0200

    Prepare for final krp test

--- scripts/process_bag.py
+++ scripts/process_bag.py
@@ -1,12 +1,6 @@
 from __future__ import print_function
 
-from data_preprocessing import PreprocessedData, Measurement
-from utils.structures import mesh, pose_from_array
-from utils.load import load_data, save
 from utils.data_processing import rosbag_inspect, rosbag_croptime, rosbag_concatenate
-from localization.models import hGP
-from krp_localization.msg import RSSIData
-
 import rospkg
 
 rospack = rospkg.RosPack()
@@ -20,9 +14,9 @@ output_bag3 = rospack.get_path('krp_localization') + '/bags/krp1-3.bag'
 krp_bag1    = rospack.get_path('krp_localization') + '/bags/krp1.bag'
 krp_bag2    = rospack.get_path('krp_localization') + '/bags/krp2.bag'
 
-#rosbag_croptime(file_path, output_bag1, tend=1565603530.0)
-#rosbag_croptime(file_path, output_bag2, tstart=1565603580, tend=1565603650)
-#rosbag_croptime(file_path, output_bag3, tstart=1565603680, tend=1565603720)
+rosbag_croptime(file_path, output_bag1, tend=1565603518)
+rosbag_croptime(file_path, output_bag2, tstart=1565603580, tend=1565603650)
+rosbag_croptime(file_path, output_bag3, tstart=1565603680, tend=1565603720)
 #rosbag_inspect(output_bag1)
 #rosbag_inspect(output_bag2)
 #rosbag_inspect(output_bag3)
---
+++ scripts/train_model.py
@@ -0,0 +1,43 @@
+from localization.models import hGP
+from utils.load import load_data
+from data_preprocessing import PreprocessedData
+from localization.sampling import accept_reject_by_regions, accept_reject_by_regions_map, accept_reject_uniform
+
+import rospkg
+import numpy as np
+
+rospack = rospkg.RosPack()
+
+train_data_prefix = "wholemap"
+train_data_path   = rospack.get_path('krp_localization') + '/data/'
+save_path         = rospack.get_path('krp_localization') + '/models/model_wholemap.p'
+
+print('Loading train data path: {}'.format(train_data_path))
+raw_rssi, poses = load_data(file_name=train_data_prefix, file_path=train_data_path) #odometry is not needed
+
+print('Number of RSSI dataset         : {}'.format(len(raw_rssi)))
+print('Number of AMCL poses           : {}'.format(len(poses)))
+
+traindata = PreprocessedData(raw_rssi, flag_negative_db         = True,
+                                       flag_min_distance        = False,
+                                       flag_fuse_measurements   = True,
+                                       flag_min_points_per_AP   = False,
+                                       flag_mode_filter         = False,
+                                       flag_discard_non_pose    = True,
+                                       poses                    = poses,
+                                       filter_fuse_measurements = 2)
+
+distance = np.sum(traindata.data['X'][1:]**2 + traindata.data['X'][:-1]**2 - 2*traindata.data['X'][1:]*traindata.data['X'][:-1],axis=1)**.5
+avg_distance = np.mean(distance)
+
+print('Number of processed measurements: {}'.format(traindata.data["Y"].shape[0]))
+print('Number of macs considered (all) : {}'.format(len(traindata.all_mac_dict)))
+print('Average distance between points : {}'.format(np.round(avg_distance,2)))
+
+#Train model
+print('Training model')
+model = hGP(traindata.data,all_mac_dict=traindata.all_mac_dict, sampling=accept_reject_uniform, Xtest_num=75)
+model.optimize()
+print('Model trained successfully')
+
+model.save(save_path)
