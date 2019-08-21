e5220ba62990d7431ba54fa4714e598752917a9a
anindex <eeit2015_an.lt@student.vgu.edu.vn>
2019-08-21 16:28:41 +0200

    Behavior for krp

--- scripts/process_bag.py
+++ scripts/process_bag.py
@@ -4,7 +4,7 @@ from utils.data_processing import rosbag_inspect, rosbag_croptime, rosbag_concat
 import rospkg
 
 rospack = rospkg.RosPack()
-file_path = rospack.get_path('krp_localization') + '/bags/test2.bag'
+file_path = rospack.get_path('krp_localization') + '/bags/test4.bag'
 #rosbag_inspect(file_path)
 
 output_bag1 = rospack.get_path('krp_localization') + '/bags/krp1-1.bag'
@@ -14,12 +14,12 @@ output_bag3 = rospack.get_path('krp_localization') + '/bags/krp1-3.bag'
 krp_bag1    = rospack.get_path('krp_localization') + '/bags/krp1.bag'
 krp_bag2    = rospack.get_path('krp_localization') + '/bags/krp2.bag'
 
-#rosbag_croptime(file_path, output_bag1, tend=1565963560)
-#rosbag_croptime(file_path, output_bag2, tstart=1565963640, tend=1565963700)
-#rosbag_croptime(file_path, output_bag3, tstart=1565603915, tend=1565603960)
-#rosbag_inspect(output_bag1)
-#rosbag_inspect(output_bag2)
-#rosbag_inspect(output_bag3)
+rosbag_croptime(file_path, output_bag1, tend=1566228135)
+rosbag_croptime(file_path, output_bag2, tstart=1566228160, tend=1566228230)
+rosbag_croptime(file_path, output_bag3, tstart=1566228250, tend=1566228310)
+rosbag_inspect(output_bag1)
+rosbag_inspect(output_bag2)
+rosbag_inspect(output_bag3)
 
 
 rosbag_concatenate(output_bag1, output_bag2, krp_bag1)
